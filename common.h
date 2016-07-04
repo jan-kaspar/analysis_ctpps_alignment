5cd1a05afe8dbccf25e0e95f397465e66e2c8bd6
Jan Kašpar <jan.kaspar@cern.ch>
2016-07-04 19:00:45 +0200

    Started common.h.

---
+++ common.h
@@ -0,0 +1,6 @@
+// parameter defaults
+
+double n_si = 3.;
+
+double cut1_a = -1., cut1_c = 0., cut1_si = 0.20;
+double cut2_a = -1., cut2_c = 0., cut2_si = 0.20;
--- distributions.cc
+++ distributions.cc
@@ -1,4 +1,6 @@
 #include "input_files.h"
+#include "common.h"
+#include "parameters.h"
 
 #include "stat.h"
 
@@ -25,17 +27,6 @@
 
 using namespace std;
 using namespace edm;
-
-//----------------------------------------------------------------------------------------------------
-
-// defaults
-
-double n_si = 3.;
-
-double cut1_a = -1., cut1_c = 0., cut1_si = 0.20;
-double cut2_a = -1., cut2_c = 0., cut2_si = 0.20;
-
-#include "parameters.h"
 
 //----------------------------------------------------------------------------------------------------
 
