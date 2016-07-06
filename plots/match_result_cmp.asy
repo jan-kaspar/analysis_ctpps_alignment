b1810c5b3176086f869acf65cf9ca325aa7392d6
Jan Kašpar <jan.kaspar@cern.ch>
2016-07-06 18:58:50 +0200

    Added more data - unconfigured so far.

--- plots/match_result_cmp.asy
+++ plots/match_result_cmp.asy
@@ -8,6 +8,12 @@ string datasets[] = {
 	"run_physics_margin/274241",
 	"run_physics_no_margin/274244",
 	"run_physics_no_margin/274388",
+	"run_physics_no_margin/274958",
+	"run_physics_no_margin/274969",
+	"run_physics_no_margin/275125",
+	"run_physics_no_margin/275310",
+	"run_physics_no_margin/275376",
+	"run_physics_no_margin/275836",
 };
 
 string ref_label[];
---
+++ run_physics_no_margin/275310/input_files.h
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+//----------------------------------------------------------------------------------------------------
+
+std::vector<std::string> input_files;
+
+std::string input_ntuple_name;
+
+void InitInputFiles()
+{
+	input_ntuple_name = "TotemNtuple";
+
+	input_files.clear();
+
+	std::string prefix = "root://eostotem.cern.ch//eos/totem/data/ctpps/reconstruction/version3/multiple_runs_3/";
+	prefix += "275310/";
+
+	for (int idx = 0; idx <= 19; ++idx)
+	{
+		char buf[100];
+		sprintf(buf, "%i", idx);
+
+		input_files.push_back(prefix + buf + "/reco.root");
+	}
+}
