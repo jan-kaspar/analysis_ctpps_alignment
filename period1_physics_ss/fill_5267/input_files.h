5151ba8099bfd8da681cee66364067f26458612c
Jan Kašpar <jan.kaspar@cern.ch>
2017-01-09 18:45:19 +0100

    More input data.

--- period1_physics_ss/fill_5267/input_files.h
+++ period1_physics_ss/fill_5267/input_files.h
@@ -17,7 +17,7 @@ void InitInputFiles()
 	char buf[100];
    
 	prefix = "root://eostotem.cern.ch//eos/totem/data/ctpps/reduction/version3/279844/";
-	for (int idx : { 9 })
+	for (int idx : { 1, 5, 6, 7, 8, 9 })
 	{
 		sprintf(buf, "%i", idx);
 		input_files.push_back(prefix + buf + "/reco.root");
--- period1_physics_ss/fill_5267/parameters.h
+++ period1_physics_ss/fill_5267/parameters.h
@@ -2,10 +2,10 @@ void ApplySettings()
 {
 	lhc_fill = 5267;
 
-	cut1_a = -0.999; cut1_c = -0.721; cut1_si = 0.20;
-	cut2_a = -0.976; cut2_c = +0.481; cut2_si = 0.20;
-	cut3_a = -1.126; cut3_c = +0.109; cut3_si = 0.15;
-	cut4_a = -1.125; cut4_c = +0.044; cut4_si = 0.15;
+	cut1_a = -0.990; cut1_c = -0.847; cut1_si = 0.20;
+	cut2_a = -0.972; cut2_c = +0.455; cut2_si = 0.20;
+	cut3_a = -1.144; cut3_c = +0.107; cut3_si = 0.15;
+	cut4_a = -1.139; cut4_c = +0.027; cut4_si = 0.15;
 
 	selectionRangesX["L_1_F"] = SelectionRange(9.2, 16.5);
 	selectionRangesX["L_1_N"] = SelectionRange(8.3, 15.7);
