c050528bba1adc2636e2b186c0847584cf44efbd
Jan Kašpar <jan.kaspar@cern.ch>
2017-01-13 20:55:47 +0100

    Added local angles.

--- shared_track.h
+++ shared_track.h
@@ -10,14 +10,24 @@ using namespace edm;
 struct TrackData
 {
 	bool valid = false;
+
+	// track position, in mm
 	double x = 0.;
 	double y = 0.;
+
+	// track angle, in rad
+	double th_x = 0.;
+	double th_y = 0.;
 
 	void operator= (const TotemRPLocalTrack &ft)
 	{
 		valid = ft.isValid();
+
 		x = ft.getX0();
 		y = ft.getY0();
+
+		th_x = ft.getTx();
+		th_y = ft.getTy();
 	}
 };
 
